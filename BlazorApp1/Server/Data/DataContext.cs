0329c92eefef3fb93877c617c807bd6e2f2afd14
Zodiark619 <seonghyejin33@gmail.com>
2022-07-15 13:35:12 +0700

    Add project files.

---
+++ BlazorApp1/Server/Data/DataContext.cs
@@ -0,0 +1,44 @@
+﻿
+namespace BlazorApp1.Server.Data
+{
+	public class DataContext:DbContext
+	{
+		public DataContext(DbContextOptions<DataContext> options):base(options)
+		{
+
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Product>().HasData(
+				new Product
+				{
+					Id = 1,
+					Title = "Hithicker's Guide to GALAZZY",
+					Description = "kmzkmzkmzkzmkzmkzmkz",
+					ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/5/5e/Domestic_Cat_Face_Shot.jpg",
+					Price = 9.99m
+				},
+		new Product
+		{
+			Id = 2,
+			Title = "Bucin KKKKmz",
+			Description = "Ready play sefnefnef",
+			ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/4/4f/Felis_silvestris_catus_lying_on_rice_straw.jpg",
+			Price = 7.99m
+		},new Product
+		{
+			Id = 3,
+			Title = "Konto konto Neonn KssssI",
+			Description = "HJHHjasjdajs",
+			ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/b/bb/Kittyply_edit1.jpg",
+			Price = 19.99m
+		}
+			
+			);
+		}
+
+
+		public DbSet<Product> Products { get; set; }
+	}
+}
---
+++ BlazorApp1/Server/Migrations/20220715062047_ProductSeeding.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorApp1.Server.Migrations
+{
+    public partial class ProductSeeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 1, "kmzkmzkmzkzmkzmkzmkz", "https://upload.wikimedia.org/wikipedia/commons/5/5e/Domestic_Cat_Face_Shot.jpg", 9.99m, "Hithicker's Guide to GALAZZY" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 2, "Ready play sefnefnef", "https://upload.wikimedia.org/wikipedia/commons/4/4f/Felis_silvestris_catus_lying_on_rice_straw.jpg", 7.99m, "Bucin KKKKmz" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 3, "HJHHjasjdajs", "https://upload.wikimedia.org/wikipedia/commons/b/bb/Kittyply_edit1.jpg", 19.99m, "Konto konto Neonn KssssI" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
